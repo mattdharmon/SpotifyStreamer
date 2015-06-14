3b200ba246c15b66f9b77e6aad9e48a84a8b1651
Matthew Harmon <matt.harmon@skillset.com>
2015-06-14 09:48:44 -0500

    Wrapped the Spotify API requests in try catchs.

--- app/src/main/java/us/gingertech/spotifystreamer/spotify/api/task/FetchArtistsAsyncTask.java
+++ app/src/main/java/us/gingertech/spotifystreamer/spotify/api/task/FetchArtistsAsyncTask.java
@@ -1,6 +1,7 @@
 package us.gingertech.spotifystreamer.spotify.api.task;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -8,37 +9,55 @@ import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.RetrofitError;
 
 /**
+ * Fetch an ArrayList of Artists from spotify, async style.
  * Created by Matthew Harmon on 6/13/15.
  */
 public class FetchArtistsAsyncTask extends AsyncTask<String, Void, ArrayList<Artist>> {
-    private SpotifyApi api;
     private SpotifyService service;
     private IOnTaskCompleted listener;
 
     public FetchArtistsAsyncTask(IOnTaskCompleted listener) {
         this.listener = listener;
-        api = new SpotifyApi();
-        service = api.getService();
+        service = new SpotifyApi().getService();
     }
 
     @Override
     protected ArrayList<Artist> doInBackground(String... params) {
-        if (params.length == 0) {
-            return null;
-        }
+        try {
+            if (params.length == 0) {
+                throw new Exception("Params are not to be empty.");
+            }
 
-        ArtistsPager artistsPager = service.searchArtists(params[0]);
-        return (ArrayList<Artist>) artistsPager.artists.items;
+            // Execute the service.
+            ArtistsPager artistsPager = service.searchArtists(params[0]);
+
+            return (ArrayList<Artist>) artistsPager.artists.items;
+        } catch (RetrofitError e) {
+            Log.d("Retrofit Error: ", e.toString());
+        } catch (Exception e) {
+            Log.d("Missing Params: ", e.toString());
+        }
+        return null;
     }
 
     @Override
     protected void onPostExecute(ArrayList<Artist> artists) {
+        // If an error occured, return it to the user.
+        if (artists == null) {
+            listener.onTaskFailure("Error.");
+            return;
+        }
+
+        // Check to see if any artists are returned.
         if (artists.isEmpty()) {
             listener.onTaskFailure("No results found.");
             return;
         }
+
+        // Return the list of artists to the listener.
         listener.onTaskCompleted(artists);
     }
 }
--- app/src/main/java/us/gingertech/spotifystreamer/spotify/api/task/FetchArtistsTopTracksAsyncTask.java
+++ app/src/main/java/us/gingertech/spotifystreamer/spotify/api/task/FetchArtistsTopTracksAsyncTask.java
@@ -1,6 +1,7 @@
 package us.gingertech.spotifystreamer.spotify.api.task;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,6 +11,7 @@ import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Track;
 import kaaes.spotify.webapi.android.models.Tracks;
+import retrofit.RetrofitError;
 
 /**
  * Created by Matthew Harmon on 6/13/15.
@@ -26,20 +28,42 @@ public class FetchArtistsTopTracksAsyncTask extends AsyncTask<String, Void, Arra
 
     @Override
     protected ArrayList<Track> doInBackground(String... params) {
-        // Build the country query params
-        Map<String,Object> query = new HashMap<>();
-        query.put("country", "US");
+        try {
+            if (params.length == 0) {
+                throw new Exception("Params are not to be empty.");
+            }
 
-        Tracks tracks = service.getArtistTopTrack(params[0], query);
-        return (ArrayList<Track>) tracks.tracks;
+            // Build the country query params
+            Map<String,Object> query = new HashMap<>();
+            query.put("country", "US");
+
+            // Execute the api service.
+            Tracks tracks = service.getArtistTopTrack(params[0], query);
+
+            return (ArrayList<Track>) tracks.tracks;
+        } catch (RetrofitError e) {
+            Log.d("Retrofit Error:", e.toString());
+        } catch (Exception e) {
+            Log.d("Missing Params: ", e.toString());
+        }
+        return null;
     }
 
     @Override
     protected void onPostExecute(ArrayList<Track> tracks) {
+        // If an error occurred, return it to the user.
+        if (tracks == null) {
+            listener.onTaskFailure("Error.");
+            return;
+        }
+
+        // Check to see if any tracks are returned.
         if (tracks.isEmpty()) {
             listener.onTaskFailure("No artist was found.");
             return;
         }
+
+        // Trigger the listener with the tracks.
         listener.onTaskCompleted(tracks);
     }
 }
